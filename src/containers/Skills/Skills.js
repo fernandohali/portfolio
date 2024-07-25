027641ab373d01816ec40159dc097fc6781e8398
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-07-25 09:12:23 -0300

    upgrade

--- src/containers/Skills/Skills.js
+++ src/containers/Skills/Skills.js
@@ -1,15 +1,85 @@
-import React from "react";
-import { Container, MySkills, Experiencia, BoxSkills } from "./Styles.js";
+import React, { useEffect, useRef } from "react";
+import {
+  Container,
+  MySkills,
+  Experiencia,
+  SkillsWrapper,
+  BoxSkill,
+  TituloSkill,
+  InforSkill,
+  Img,
+} from "./Styles";
 
 export function Skills() {
+  const skillsRef = useRef(null);
+
+  useEffect(() => {
+    const scrollContainer = skillsRef.current;
+    let scrollAmount = 0;
+    const scrollStep = 1.3; // Ajuste a velocidade de rolagem aqui
+    const scrollInterval = setInterval(() => {
+      if (scrollAmount >= scrollContainer.scrollWidth / 2) {
+        scrollAmount = 0; // Reinicia o scroll
+      } else {
+        scrollAmount += scrollStep;
+      }
+      scrollContainer.scrollLeft = scrollAmount;
+    }, 50); // Ajuste a frequência de rolagem aqui (milissegundos)
+
+    return () => clearInterval(scrollInterval); // Limpa o intervalo quando o componente é desmontado
+  }, []);
+  const skills = [
+    {
+      title: "Estratégia & Direção",
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+    },
+    {
+      title: "Desenvolvimento de Jogos",
+      info: `- Projeto: ValorantGuia
+Linguagem: Java Android
+<a href="https://github.com/fernandohali/ValorantGuia" target="_blank" rel="noopener noreferrer">GitHub Link</a>
+Descrição: Desenvolvimento de um aplicativo Android chamado ValorantGuia, projetado para servir como um guia detalhado para o jogo Valorant. O aplicativo apresenta informações sobre os mapas do jogo e a localização dos agentes. Além disso, inclui vídeos que mostram os melhores pontos de referência (pixels) nos mapas para estratégias de jogo.
+      `,
+    },
+    {
+      title: "Desenvolvimento Mobile",
+      info: ` - Linguagem Mobile: React Native
+- Projeto: Gerenciamento de sala
+Linguagem: JavaScript, Framework: React Native 
+<a href="https://github.com/EngenhariaSoftware123/Gest-o-salas-front-nativo" target="_blank" rel="noopener noreferrer">GitHub Link</a>
+Descrição: Desenvolvimento de um aplicativo móvel utilizando React Native para gerenciar as salas da Universidade Estadual do Sudoeste da Bahia (UESB). O aplicativo foi projetado para facilitar o controle e a organização das salas de aula, permitindo que usuários reservem, consultem e gerenciem as salas de maneira eficiente.
+      `,
+    },
+    {
+      title: "Desenvolvimento Web",
+      info: `
+        - Desenvolvimento Web: HTML, CSS, JavaScript
+        - Frameworks: React.js, Spring Boot, Node.js
+      `,
+    },
+    {
+      title: "Branding & Logo",
+      info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+    },
+  ];
+
+  const createMarkup = (html) => {
+    return { __html: html };
+  };
+
   return (
     <Container>
-      <MySkills>Minhas habilidades</MySkills>
-      <Experiencia>Minha experiência</Experiencia>
-      <BoxSkills>
-
-        
-      </BoxSkills>
+      <MySkills>My Skills</MySkills>
+      <Experiencia>My Expertise</Experiencia>
+      <SkillsWrapper ref={skillsRef}>
+        {skills.concat(skills).map((skill, index) => (
+          <BoxSkill key={index}>
+            <Img src={skill.imgSrc} alt={skill.title} />
+            <TituloSkill>{skill.title}</TituloSkill>
+            <InforSkill dangerouslySetInnerHTML={createMarkup(skill.info)} />
+          </BoxSkill>
+        ))}
+      </SkillsWrapper>
     </Container>
   );
 }
--- src/containers/Skills/Styles.js
+++ src/containers/Skills/Styles.js
@@ -2,22 +2,19 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   background-color: white;
-
   padding: 2rem;
   margin: 0 auto;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-    text-align: center;
-  }
+  max-width: 1200px;
 `;
 
 export const MySkills = styled.h1`
-  font-size: 13px;
+  font-size: 1rem;
   color: black;
   font-weight: bold;
   margin: 10px;
   background-color: transparent;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
 
   @media (max-width: 466px) {
     font-size: 1rem;
@@ -26,11 +23,10 @@ export const MySkills = styled.h1`
 `;
 
 export const Experiencia = styled.h2`
-  font-size: 30px;
+  font-size: 2rem;
   font-weight: bold;
   margin-top: 10px;
   margin: 10px;
-
   background-color: transparent;
   color: #282938;
 
@@ -39,7 +35,61 @@ export const Experiencia = styled.h2`
     margin-bottom: 1rem;
   }
 `;
-export const BoxSkills = styled.div`
-  @media (max-width: 466px) {
+
+export const SkillsWrapper = styled.div`
+  display: flex;
+  overflow-x: hidden; /* Oculta a rolagem horizontal */
+  padding: 20px 0;
+  white-space: nowrap; /* Impede que o conteúdo se quebre em múltiplas linhas */
+
+  &::-webkit-scrollbar {
+    display: none; /* Oculta a barra de rolagem */
+  }
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
+
+export const BoxSkill = styled.div`
+  background-color: #f0f4f8;
+  border-radius: 8px;
+  padding: 20px;
+  margin: 10px;
+  flex: 0 0 auto;
+  width: 250px;
+  text-align: center;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+export const Img = styled.img`
+  width: 50px;
+  height: 50px;
+  margin-bottom: 20px;
+`;
+
+export const TituloSkill = styled.h3`
+  font-size: 18px;
+  font-weight: bold;
+  color: #282938;
+  margin-bottom: 10px;
+`;
+
+export const InforSkill = styled.p`
+  font-size: 14px;
+  color: #6b7280;
+  white-space: pre-wrap;
+  text-align: left;
+
+  a {
+    color: blue;
+    text-decoration: underline;
+
+    &:hover {
+      text-decoration: none;
+    }
   }
 `;
