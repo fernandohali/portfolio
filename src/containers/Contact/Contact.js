5219640ac32ec0afd4af794c5cba944cfbb15bc5
Fernando Hali <100550886+fernandohali@users.noreply.github.com>
2024-08-01 21:07:28 -0300

    Finalizando contact

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -9,9 +9,13 @@ import {
   TextArea,
   Select,
   Button,
+  NameContainer,
+  MaskedInput,
 } from "./Styles.js";
 
+// Função principal do componente de contato
 export function Contact() {
+  // Hook useState para gerenciar os dados do formulário
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -22,24 +26,33 @@ export function Contact() {
     acceptTerms: false,
   });
 
+  // Função para lidar com a mudança dos campos do formulário
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
+    // Atualiza o estado do formData com os novos valores dos campos
     setFormData((prevData) => ({
       ...prevData,
       [name]: type === "checkbox" ? checked : value,
     }));
   };
 
+  // Função para lidar com o envio do formulário
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Dados do formulário enviados:", formData);
 
+    // Envia o formulário utilizando a biblioteca emailjs
     emailjs
       .send(
+        "service_0mpvwrk", // ID do serviço do emailjs
+        "template_w8ur8f9", // ID do template do emailjs
+        formData,
+        "X-cSeGjVwC4U1RmoP" // Chave pública do emailjs
       )
       .then(
         (response) => {
           console.log("EMAIL ENVIADO", response.status, response.text);
+          // Limpa o formulário após o envio bem-sucedido
           setFormData({
             firstName: "",
             lastName: "",
@@ -56,27 +69,30 @@ export function Contact() {
       );
   };
 
+  // Renderiza o formulário de contato
   return (
     <Container>
       <Title>Entrar em contato</Title>
       <Subtitle>Contate-me</Subtitle>
       <Form onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          name="firstName"
-          placeholder="Primeiro nome"
-          value={formData.firstName}
-          onChange={handleChange}
-          required
-        />
-        <Input
-          type="text"
-          name="lastName"
-          placeholder="Último nome"
-          value={formData.lastName}
-          onChange={handleChange}
-          required
-        />
+        <NameContainer>
+          <Input
+            type="text"
+            name="firstName"
+            placeholder="Primeiro nome"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+          />
+          <Input
+            type="text"
+            name="lastName"
+            placeholder="Último nome"
+            value={formData.lastName}
+            onChange={handleChange}
+            required
+          />
+        </NameContainer>
         <Input
           type="email"
           name="email"
@@ -85,7 +101,8 @@ export function Contact() {
           onChange={handleChange}
           required
         />
-        <Input
+        <MaskedInput
+          mask="(99) 99999-9999"
           type="tel"
           name="phoneNumber"
           placeholder="Número de telefone"
--- src/containers/Contact/Styles.js
+++ src/containers/Contact/Styles.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+import InputMask from "react-input-mask";
 
 export const Container = styled.div`
   padding: 2rem;
   margin: 0 auto;
+  margin-bottom: 20px;
   max-width: 600px; /* Ajuste a largura máxima conforme necessário */
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border: 1px solid blue;
 `;
 
 export const Title = styled.h2`
@@ -27,6 +31,30 @@ export const Input = styled.input`
   border: 1px solid #ccc;
   border-radius: 4px;
   font-size: 1rem;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border: 1px solid blue;
+`;
+
+//
+export const MaskedInput = styled(InputMask)`
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 1rem;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border: 1px solid blue;
+`;
+
+export const Select = styled.select`
+  padding: 0.5rem;
+  margin-bottom: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 1rem;
+
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border: 1px solid blue;
 `;
 
 export const TextArea = styled.textarea`
@@ -36,24 +64,9 @@ export const TextArea = styled.textarea`
   border-radius: 4px;
   font-size: 1rem;
   height: 100px;
-`;
 
-export const Select = styled.select`
-  padding: 0.5rem;
-  margin-bottom: 1rem;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 1rem;
-`;
-
-export const Checkbox = styled.label`
-  display: flex;
-  align-items: center;
-  margin-bottom: 1rem;
-
-  input {
-    margin-right: 0.5rem;
-  }
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border: 1px solid blue;
 `;
 
 export const Button = styled.button`
@@ -64,8 +77,27 @@ export const Button = styled.button`
   color: white;
   font-size: 1rem;
   cursor: pointer;
-
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border: 1px solid blue;
   &:hover {
     background-color: #0056b3;
+  }
+`;
+
+export const NameContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+
+  ${Input} {
+    flex: 1;
+  }
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+
+    ${Input} {
+      margin-right: 0;
+      margin-bottom: 1rem;
+    }
   }
 `;
